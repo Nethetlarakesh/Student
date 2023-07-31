7d7b349aed672e5d9cbfd3aa8c2e2204ee5a0f97
Nethetlarakesh <93771254+Nethetlarakesh@users.noreply.github.com>
2023-07-31 11:42:23 +0530

    Add files via upload

---
+++ SpringAppStudent3/src/main/java/com/rakesh/App.java
@@ -0,0 +1,18 @@
+package com.rakesh;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      ApplicationContext ctx=new ClassPathXmlApplicationContext("spconfig.xml");
+      EmployeeBean a=(EmployeeBean)ctx.getBean("id1");
+      a.printEmployee();
+    } 
+}
